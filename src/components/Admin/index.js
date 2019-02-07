49b3ea73bdd10410d35b00ff0b1324e88e68814e
Ionut Pistol <ipistol@pentalog.com>
2019-02-07 19:02:13 +0200

    Restored Admin page

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-const Admin = () => <div>Admin page - Added. What is your product price?</div>;
+const Admin = () => <div>Admin page - What is your product price?</div>;
 
 export default Admin;
